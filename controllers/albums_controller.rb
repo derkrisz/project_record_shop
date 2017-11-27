63381a9e7382ba92674bc53b1a787070939a61e2
derkrisz <krisztian.dergmail.com>
2017-11-27 13:09:21 +0000

    create album functions

--- controllers/albums_controller.rb
+++ controllers/albums_controller.rb
@@ -1,3 +1,18 @@
 require('sinatra')
 require('sinatra/reloader')
 require_relative('../models/album.rb')
+require_relative('../models/artist.rb')
+
+
+#NEW
+get '/album/new' do
+  @artists = Artist.all
+  erb( :"album/new" )
+end
+
+#CREATE
+post '/album' do
+  @album = Album.new( params )
+  @album.save()
+erb ( :"album/create" )
+end
