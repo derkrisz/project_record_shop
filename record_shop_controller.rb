c0783405c60f59f8064e234a991f1fac2ecacf46
derkrisz <krisztian.dergmail.com>
2017-11-27 13:08:26 +0000

    home page stock counter functions

--- record_shop_controller.rb
+++ record_shop_controller.rb
@@ -4,7 +4,9 @@ require('sinatra/reloader')
 
 require_relative('./controllers/albums_controller.rb')
 require_relative('./controllers/artists_controller.rb')
+require_relative('./models/album.rb')
 
 get '/' do
+  @stock = Album.stock
   erb ( :index )
 end
