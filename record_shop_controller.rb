6fb630003f355bc505fb6992561fbedb500aed24
derkrisz <krisztian.dergmail.com>
2017-11-27 11:56:29 +0000

    modifies record shop controller

--- record_shop_controller.rb
+++ record_shop_controller.rb
@@ -2,42 +2,9 @@ require('sinatra')
 require('sinatra/reloader')
 
 
-require_relative('./models/album.rb')
-require_relative('./models/artist.rb')
+require_relative('./controllers/albums_controller.rb')
+require_relative('./controllers/artists_controller.rb')
 
-#DEFAULT
-get '/default' do
-  erb ( :default )
-end
-
-#INDEX
-get '/default/index' do
-  @artists = Artist.all
-  @stock = Album.stock
+get '/' do
   erb ( :index )
-end
-
-#NEW
-get '/default/new' do
-  erb( :new )
-end
-
-#SHOW
-get '/default/index/:id' do
-  @albums = Album.find( params[:id] )
-erb ( :show )
-end
-
-#CREATE
-post '/default' do
-  @artist = Artist.new( params )
-  @artist.save()
-erb ( :default )
-end
-
-#DELETE
-delete '/default/index/:id' do
-  artist = Artist.find( params[:id] )
-  artist.delete()
-  redirect to '/default/index'
 end
