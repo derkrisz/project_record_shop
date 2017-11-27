63b89f05a8017ac8c586b94cbd6ed5263c414ce5
derkrisz <krisztian.dergmail.com>
2017-11-27 19:40:50 +0000

    fixes delete artist with albums

--- db/record_shop.sql
+++ db/record_shop.sql
@@ -10,5 +10,5 @@ CREATE TABLE albums (
   id serial4 PRIMARY KEY,
   title VARCHAR(255),
   quantity INT4,
-  artist_id int4 REFERENCES artists(id)
+  artist_id int4 REFERENCES artists(id) ON DELETE CASCADE
 );
--- record_shop_controller.rb
+++ record_shop_controller.rb
@@ -1,7 +1,6 @@
 require('sinatra')
 require('sinatra/reloader')
 
-
 require_relative('./controllers/albums_controller.rb')
 require_relative('./controllers/artists_controller.rb')
 require_relative('./models/album.rb')
