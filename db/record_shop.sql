5d7535deb03f1d80b086adb378b854cc19f9631e
derkrisz <krisztian.dergmail.com>
2017-11-28 14:05:27 +0000

    fixes syntax error in db

--- db/record_shop.sql
+++ db/record_shop.sql
@@ -10,8 +10,8 @@ CREATE TABLE albums (
   id serial4 PRIMARY KEY,
   title VARCHAR(255),
   quantity INT4,
-  artist_id int4 REFERENCES artists(id) ON DELETE CASCADE
-  buy_price INT4
-  sell_price INT4
+  artist_id int4 REFERENCES artists(id) ON DELETE CASCADE,
+  buy_price INT4,
+  sell_price INT4,
   genre VARCHAR(255)
 );
