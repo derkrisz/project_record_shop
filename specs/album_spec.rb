6daf951e24227c93e026ca513ff64072f380980b
derkrisz <krisztian.dergmail.com>
2017-11-29 21:23:11 +0000

    test stock level passes

--- specs/album_spec.rb
+++ specs/album_spec.rb
@@ -2,7 +2,32 @@ require('minitest/autorun')
 require('minitest/rg')
 require_relative('../models/album.rb')
 
-class testAlbum < MiniTest::Test
+class TestAlbum < MiniTest::Test
+
+  def setup
+    @album1 = Album.new({
+      'title' => 'Davidian',
+      'quantity' => 5,
+      'buy_price' => 10,
+      'sell_price' => 15,
+      'genre' => 'metal'
+      })
+
+    @album2 = Album.new({
+      'title' => 'The More Things Change',
+      'quantity' => 1,
+      'buy_price' => 11,
+      'sell_price' => 16,
+      'genre' => 'metal'
+      })
+    end
 
 
-end
+      def test_stock_level
+        result = @album1.stock_level
+        result2 = @album2.stock_level
+        assert_equal('Medium stock', result)
+        assert_equal('Low stock', result2)
+      end
+
+    end
